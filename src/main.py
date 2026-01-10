73af51233b158aa8a51646b4bfe174c20b7aa423
Kalloreinder <matthieu.perrelle@gmail.com>
2026-01-10 09:24:52 +0100

    Created initial project structure and textnode. Tested functionality.

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from textnode import *
+
+
+def main():
+    text_node = TextNode("Some dummy text", TextType.CODE_TEXT, "www.google.com")
+    print(text_node)
+    return 0
+
+
+if __name__ == "__main__":
+    main()
