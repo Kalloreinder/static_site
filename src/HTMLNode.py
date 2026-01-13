a2fc416ef6be34a6282c7bf58b990faf35bcfbcf
Kalloreinder <matthieu.perrelle@gmail.com>
2026-01-13 20:20:40 +0100

    Added ParentNode and ran some tests

--- src/HTMLNode.py
+++ src/HTMLNode.py
@@ -32,10 +32,26 @@ class LeafNode(HTMLNode):
 
     def to_html(self):
         if self.value is None:
-            raise ValueError
+            raise ValueError("value cannot be None")
         if self.tag is None:
             return self.value
         return f"<{self.tag}>{self.value}</{self.tag}>"
     
     def __repr__(self):
         return f"LeafNode({self.tag}, {self.value}, {self.props})"
+    
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("tag cannot be None")
+        if self.children is None:
+            raise ValueError("children cannot be empty")
+        node_and_children = f"<{self.tag}>"
+        for child in self.children:
+            node_and_children += child.to_html()
+        node_and_children += f"</{self.tag}>"
+        return node_and_children
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from HTMLNode import HTMLNode, LeafNode
+from HTMLNode import HTMLNode, LeafNode, ParentNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -51,6 +51,21 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_no_children(self):
         node = LeafNode("p", "Hello World!", [HTMLNode()])
         self.assertRaises(ValueError)
+    
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
+
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>"
+        )
+        print(parent_node.to_html())
 
 if __name__ == "__main__":
     unittest.main()
